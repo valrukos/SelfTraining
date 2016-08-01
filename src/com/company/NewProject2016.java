3b5cacc8d7503a6f52b4b148412808b1705dcd91
valrukos <valrukos@hotmail.ca>
2016-08-01 18:12:29 -0400

    Temp changes to  new 2016 project

---
+++ src/com/company/NewProject2016.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by Owner on 8/1/16.
+ */
+public class NewProject2016 {
+    public static void main(String[] args) {
+        System.out.println("Changes to new project 2016");
+    }
+}
