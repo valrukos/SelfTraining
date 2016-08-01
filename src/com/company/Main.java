eb9ef7386dfe956dce277ec1a7545dc0f5fb9597
valrukos <valrukos@hotmail.ca>
2016-08-01 18:40:10 -0400

    commit with before commit functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.err.println("New branch 1");
         System.err.println("Start new 2016 project");
         System.out.println("Continue working with project");
+        System.out.println("Implement new feature");
     }
 }
